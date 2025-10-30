a447db8e2f7a7050fcbff33d2730fe44c96f10a7
dimka <dimka556.dim4@gmail.com>
2025-10-30 23:30:54 +0500

    Task1

---
+++ Tyuiu.AytuvarovTK.Sprint4.Task1.V21.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.AytuvarovTK.Sprint4.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
