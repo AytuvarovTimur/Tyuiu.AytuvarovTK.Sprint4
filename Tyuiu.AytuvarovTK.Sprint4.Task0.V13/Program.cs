5e1dfe583ae8ae2e734ba52ce6114d140996f57e
dimka <dimka556.dim4@gmail.com>
2025-10-30 23:19:14 +0500

    Add project files.

---
+++ Tyuiu.AytuvarovTK.Sprint4.Task0.V13/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AytuvarovTK.Sprint4.Task0.V13.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Tyuiu.LavrentevVA.Sprint4.Task0.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+            int sumEvenElements = ds.GetSumEvenArrEl(numsArray);
+            Console.WriteLine($"Сумма четных элементов массива: {sumEvenElements}");
+            Console.ReadKey();
+
+        }
+    }
+}
