0c0af5924eb4b8f8260548551590ea37354ddc57
dimka <dimka556.dim4@gmail.com>
2025-10-31 00:10:20 +0500

    Task3

--- Tyuiu.AytuvarovTK.Sprint4.Task3.V9.Test/Test1.cs
+++ Tyuiu.AytuvarovTK.Sprint4.Task3.V9.Test/Test1.cs
@@ -1,4 +1,4 @@
-﻿namespace Tyuiu.AytuvarovTK.Sprint4.Task3.V30.Test
+﻿namespace Tyuiu.AytuvarovTK.Sprint4.Task3.V9.Test
 {
     [TestClass]
     public sealed class Test1
--- Tyuiu.AytuvarovTK.Sprint4.Task3.V9/Program.cs
+++ Tyuiu.AytuvarovTK.Sprint4.Task3.V9/Program.cs
@@ -1,10 +1,10 @@
-﻿using Tyuiu.AytuvarovTK.Sprint4.Task3.V30.Lib;
+﻿using Tyuiu.AytuvarovTK.Sprint4.Task3.V9.Lib;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 
-namespace Tyuiu.AytuvarovTK.Sprint4.Task3.V30
+namespace Tyuiu.AytuvarovTK.Sprint4.Task3.V9
 {
     internal class Program
     {
@@ -13,15 +13,15 @@ namespace Tyuiu.AytuvarovTK.Sprint4.Task3.V30
             DataService ds = new DataService();
 
             int len;
-            Console.WriteLine("Введите размер массива (5):");
+            Console.WriteLine("Введите длину массива (9):");
             len = Convert.ToInt32(Console.ReadLine());
-            int[,] numsArray = new int[len, len];
+            int [,] numsArray = new int[len,len];
             for (int i = 0; i < len; i++)
             {
                 for (int j = 0; j < len; j++)
                 {
                     Console.WriteLine($"Введите элемент массива [{i + 1},{j + 1}] (от 3 до 9):");
-                    numsArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                    numsArray[i,j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
             Console.WriteLine("Исходный массив");
@@ -29,13 +29,15 @@ namespace Tyuiu.AytuvarovTK.Sprint4.Task3.V30
             {
                 for (int j = 0; j < len; j++)
                 {
-                    Console.Write(numsArray[i, j] + " ");
+                    Console.Write(numsArray[i,j] + " ");
                 }
                 Console.WriteLine();
             }
             int result = ds.Calculate(numsArray);
-            Console.WriteLine("Сумма элементов во втором столбце массива:");
+            Console.WriteLine("Сумма элементов второго столбца массива:");
             Console.WriteLine(result);
+
+
             Console.ReadKey();
 
         }
