71dd69e36d02c3d48264e0477d4e76c22d01ff37
dimka <dimka556.dim4@gmail.com>
2025-10-31 20:23:12 +0500

    Task6

---
+++ Tyuiu.AytuvarovTK.Sprint4.Task6.V13.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AytuvarovTK.Sprint4.Task6.V13.Lib
+{
+    public class DataService : ISprint4Task6V13
+    {
+        public int Calculate(string[] array)
+        {
+            // Дан строковый массив данных ["Ford", "Toyota", "Honda", "Chevrolet", "Mercedes", "BMW", "Audi"] используя класс Array подсчитайте количество элементов, длина которых больше 4.
+            int count = 0;
+            foreach (var item in array)
+            {
+                if (item.Length > 4)
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint4.Task6.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AytuvarovTK.Sprint4.Task6.V13.Lib;
+using System;
+
+namespace Tyuiu.AytuvarovTK.Sprint4.Task6.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int len;
+            Console.WriteLine("Введите длину массива (7):");
+            len = Convert.ToInt32(Console.ReadLine());
+            string[] strArray = new string[len];
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine($"Введите элемент массива [{i + 1}]:");
+                strArray[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(strArray[i] + " ");
+            }
+            Console.WriteLine();
+
+            int result = ds.Calculate(strArray);
+            Console.WriteLine("Количество элементов, длина которых больше 4:");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
