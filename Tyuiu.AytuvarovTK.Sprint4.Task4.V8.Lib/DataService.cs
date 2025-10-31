600594f76bb4f73df9dd377886dad4c22ced4cca
dimka <dimka556.dim4@gmail.com>
2025-10-31 19:28:01 +0500

    Task4

---
+++ Tyuiu.AytuvarovTK.Sprint4.Task4.V8.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.AytuvarovTK.Sprint4.Task4.V8.Lib
+{
+    public class DataService : ISprint4Task4V8
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            // Заменить все чётные элементы массива на 1 и вернуть изменённый массив
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint4.Task4.V8/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.AytuvarovTK.Sprint4.Task4.V8.Lib;
+using System;
+
+namespace Tyuiu.AytuvarovTK.Sprint4.Task4.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int len;
+            Console.WriteLine("Введите длину массива (5 на 5):");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[,] numsArray = new int[len, len];
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.WriteLine($"Введите элемент массива [{i + 1},{j + 1}] (от 4 до 8):");
+                    numsArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.Write(numsArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int[,] result = ds.Calculate(numsArray);
+
+            Console.WriteLine("Изменённый массив");
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
