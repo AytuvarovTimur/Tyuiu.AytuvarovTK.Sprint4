cb3bb888c7836cc36607d33d7ba3990be03a4583
dimka <dimka556.dim4@gmail.com>
2025-10-31 20:52:28 +0500

    Task7

---
+++ Tyuiu.AytuvarovTK.Sprint4.Task7.V22.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AytuvarovTK.Sprint4.Task7.V22.Lib
+{
+    public class DataService : ISprint4Task7V22
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            // Дана строка из одноразрядных цифр "695324951753684". Преобразуйте ее в матрицу 5 на 3 и подсчитайте произведение четных чисел.
+            int[,] matrix = new int[n, m];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[index].ToString());
+                    index++;
+                }
+            }
+            int product = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        product *= matrix[i, j];
+                    }
+                }
+            }
+            return product;
+
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint4.Task7.V22/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.AytuvarovTK.Sprint4.Task7.V22.Lib;
+using System;
+
+namespace Tyuiu.AytuvarovTK.Sprint4.Task7.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("Введите число строк (5):");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число столбцов (3):");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите строку из одноразрядных цифр (например: 695324951753684):");
+            string value = Console.ReadLine() ?? string.Empty;
+
+            if (value.Length != n * m)
+            {
+                Console.WriteLine($"Ошибка: длина строки должна быть равна {n * m} символам.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Отобразим матрицу для пользователя
+            Console.WriteLine("Матрица:");
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(value[index] + " ");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            int result = ds.Calculate(n, m, value);
+            Console.WriteLine("Произведение чётных чисел в матрице:");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
